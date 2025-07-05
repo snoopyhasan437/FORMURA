c4c20a12d971b112815148dc8054a58f4247f010
snoopyhasan437 <hasan@usbcinteriors.com>
2025-07-05 20:03:40 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#39ff14',
+      },
+    },
+  },
+  plugins: [],
+}
